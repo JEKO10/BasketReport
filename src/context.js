6eff2bf2c7704c4084e4f22fb676c3a37c0217e2
Aleksa Bubanja <aleksa3b@gmail.com>
2022-12-26 18:55:47 +0100

    API parameters

--- src/context.js
+++ src/context.js
@@ -5,11 +5,16 @@ const AppContext = createContext();
 const AppProvider = ({ children }) => {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("players");
+  const [search, setSearch] = useState("");
 
   const fetchData = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("https://www.balldontlie.io/api/v1/players");
+      const response = await fetch(
+        `https://www.balldontlie.io/api/v1/${query}?page=${page}&search=${search}`
+      );
       const data = await response.json();
       setData(data.data);
     } catch (error) {
