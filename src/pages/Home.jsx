e1583a5155a2ede8c6138de868933f2a3da0fa62
Aleksa Bubanja <aleksa3b@gmail.com>
2023-01-04 21:28:29 +0100

    header finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Banner from "../images/banner.png";
+import Banner from "../images/lbj.jpg";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -7,11 +8,26 @@ function Home() {
       <header>
         <img src={Banner} alt="Banner" />
         <ul className="headlines">
-          <li>Games</li>
-          <li>News</li>
-          <li>Players</li>
-          <li>Stats</li>
-          <li>Teams</li>
+          <li id="head">Headlines</li>
+          <li>
+            <Link to="/players">Players</Link>
+            <p>This page retrieves all players from all seasons.</p>
+          </li>
+          <li>
+            <Link to="/teams">Teams</Link>
+            <p>This page retrieves all teams for the current season.</p>
+          </li>
+          <li>
+            <Link to="/games">Games</Link>
+            <p>Seasons are represented by the year they began.</p>
+          </li>
+          <li>
+            <Link to="/stats">Stats</Link>
+            <p>This page retrieves all stats.</p>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
         </ul>
       </header>
     </section>
